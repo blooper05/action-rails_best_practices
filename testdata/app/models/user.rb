2bfe87a5c2c03d44a0ecd17bb30c2e4402595130
blooper05 <legend.of.blooper@gmail.com>
2022-06-03 01:34:39 +0900

    ✅ test: Update testdata to be detected by `rails_best_practices`

---
+++ testdata/app/models/user.rb
@@ -0,0 +1,5 @@
+# refs. https://github.com/flyerhzm/rails_best_practices/blob/HEAD/spec/rails_best_practices/reviews/default_scope_is_evil_review_spec.rb#L12-L14
+# refs. https://github.com/flyerhzm/rails_best_practices/blob/HEAD/spec/rails_best_practices/reviews/protect_mass_assignment_review_spec.rb#L17-L18
+class User < ActiveRecord::Base
+  default_scope -> { order('created_at desc') }
+end
